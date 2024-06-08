8ff1c33d938abe4f1a7c106bfeb1a33700838627
Mr raju <ma8055965@gmail.com>
2024-06-08 11:12:40 -0700

    updated

--- src/Pages/OtherUserFollowing.jsx
+++ src/Pages/OtherUserFollowing.jsx
@@ -6,6 +6,7 @@ import {
 import { useSelector } from "react-redux";
 import { useState } from "react";
 import { toast } from "react-toastify";
+import Layout from "../Layout/Layout";
 
 const OtherUserFollowing = () => {
   const userId = useParams().id;
@@ -28,47 +29,48 @@ const OtherUserFollowing = () => {
       .catch((err) => toast.error(err?.data?.message));
   };
   return (
-    <div className="w-full min-h-screen flex flex-col gap-2 py- px-6">
-      <div className="w-full flex py-2">
-        back{" "}
-        <h2 className="text-center w-full font-semibold text-xl">Following</h2>
-      </div>
-      <div className="w-full flex py-2">
-        <input
-          type="text"
-          className="w-full p-2 bg-zinc-200 rounded-lg outline-none"
-          placeholder="Search"
-        />
-      </div>
-      {user?.following?.map((follower, index) => (
-        <div
-          key={index}
-          className="w-full justify-between h-12 flex items-center"
-        >
-          <div className="flex gap-3 h-full">
-            <Link
-              to={`/user/${follower?._id}`}
-              className="w-12 h-full rounded-full bg-zinc-300 overflow-hidden"
-            >
-              <img src={follower?.profile} alt="" className="w-full h-full" />
-            </Link>
-            <div>
-              <Link to={`/user/${follower?._id}`}>{follower.username}</Link>
-              <h2>{follower.fullName}</h2>
-            </div>
-          </div>
-          <button
-            onClick={() => follow(follower._id)}
-            hidden={follower._id === me._id}
-            className={`${
-              isFollowed ? "bg-zinc-200 text-black" : "bg-sky-500 text-white"
-            } font-bold rounded-lg p-1.5 px-4`}
-          >
-            {isFollowed ? "following" : "follow"}
-          </button>
+    <Layout>
+      <div className="w-full min-h-screen flex flex-col gap-2 py- px-6">
+        <div className="w-full flex py-2">
+          back{" "}
+          <h2 className="text-center w-full font-semibold text-xl">Following</h2>
         </div>
-      ))}
-    </div>
+        <div className="w-full flex py-2">
+          <input
+            type="text"
+            className="w-full p-2 bg-zinc-200 rounded-lg outline-none"
+            placeholder="Search"
+          />
+        </div>
+        {user?.following?.map((follower, index) => (
+          <div
+            key={index}
+            className="w-full justify-between h-12 flex items-center"
+          >
+            <div className="flex gap-3 h-full">
+              <Link
+                to={`/user/${follower?._id}`}
+                className="w-12 h-full rounded-full bg-zinc-300 overflow-hidden"
+              >
+                <img src={follower?.profile} alt="" className="w-full h-full" />
+              </Link>
+              <div>
+                <Link to={`/user/${follower?._id}`}>{follower.username}</Link>
+                <h2>{follower.fullName}</h2>
+              </div>
+            </div>
+            <button
+              onClick={() => follow(follower._id)}
+              hidden={follower._id === me._id}
+              className={`${isFollowed ? "bg-zinc-200 text-black" : "bg-sky-500 text-white"
+                } font-bold rounded-lg p-1.5 px-4`}
+            >
+              {isFollowed ? "following" : "follow"}
+            </button>
+          </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
